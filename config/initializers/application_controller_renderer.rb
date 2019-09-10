f239ef7c8a37b29d562f5b54ef99d417be0fc956
Engineer-Omerta <engineer.omerta@gmail.com>
2019-09-10 19:04:31 +0900

    [Finish]3-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '954e1f78a2857f47199c00e77d34e991e15f87483453384695d75cff8bcf4583b62a6f4b4c744c444ee162bf2bad0ddb73fbbf228c406669310f268e76c42fcc'
